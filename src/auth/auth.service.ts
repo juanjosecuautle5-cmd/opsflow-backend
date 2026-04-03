f899d5a8b5192b8a0be8cd37de9cc6f60c9955f3
Juan Jose Cuautle <juanjosecuautle5@gmail.com>
2026-04-03 12:56:52 -0700

    fix bcrypt + jwt full setup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
-import * as bcrypt from 'bcryptjs';
+import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
