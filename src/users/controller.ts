30e35b4223fb3ff77b2b4bed74735ff597b4f4c8
Juan Jose Cuautle <juanjosecuautle5@gmail.com>
2026-04-03 03:06:59 -0700

    remove guard from users

--- src/users/controller.ts
+++ src/users/controller.ts
@@ -1,19 +1,17 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsersService } from './service';
-import { JwtAuthGuard } from '../auth/jwt.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  // ✅ PÚBLICO (para crear usuario)
+  // ✅ SIN GUARD
   @Post()
   createUser(@Body() body: { email: string; password: string }) {
     return this.usersService.createUser(body);
   }
 
-  // 🔒 PROTEGIDO
-  @UseGuards(JwtAuthGuard)
+  // 🔒 SOLO AQUÍ (si quieres)
   @Get()
   getUsers() {
     return this.usersService.getUsers();
