676ee2a415b6dd90653c28279d011f36ec1daae4
Juan Jose Cuautle <juanjosecuautle5@gmail.com>
2026-04-03 13:26:19 -0700

    fix auth controller routes

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
